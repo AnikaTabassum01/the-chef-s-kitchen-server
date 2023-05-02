feefed15c8ec34bd3fafc8dcfbd4ff42156776da
Anika <anika.bd1994@gmail.com>
2023-05-02 23:13:25 +0600

    datas shows in local host

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefData = require('./Data/chefData.json');
+
 app.get('/', (req, res) =>{
     res.send('Data is coming')
 });
+
+app.get('/chefData', (req, res) =>{
+    res.send(chefData)
+})
 
 app.listen(port, () => {
     console.log(`Data API is running on port: ${port}`)
